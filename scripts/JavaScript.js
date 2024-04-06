0aaeacdb050ce8557350af9ecbb44d5da89c8129
Syrinca <165695271+Syrinca@users.noreply.github.com>
2024-04-06 17:00:44 +0800

    Add files via upload

---
+++ scripts/JavaScript.js
@@ -0,0 +1,8 @@
+<devicePixelRatio>
+  if (window.devicePixelRatio !== 1) {    
+    var dpt = window.devicePixelRatio;
+    var widthM = window.screen.width * dpt;
+    var widthH = window.screen.height * dpt;
+    document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');  
+  }
+</devicePixelRatio>
